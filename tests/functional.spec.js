a9b52cfaf4e694647865790af54e87f51cc88abe
Shanisya Lahida <shanisya.lahida@epitech.eu>
2026-04-16 17:03:23 +0200

    fix: launch functional tests with playwright

--- tests/functional.spec.js
+++ tests/functional.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from 'playwright/test';
 
 test("the opening game", async ({ page }) => {
+    await page.clock.install();
     await page.goto("http://localhost:3000/");
     const canvas = page.locator("canvas");
     await expect(canvas).toBeVisible();
