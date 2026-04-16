6d5a05c4d831b69a0688cfd108011a12a0c4f526
Shanisya Lahida <shanisya.lahida@epitech.eu>
2026-04-16 16:03:00 +0200

    fix: functional tests with playwright

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: 'tests',
-  testIgnore: 'tests/*.spec.js',
+  testDir: 'test',
+  testIgnore: 'test/*.spec.js',
 
   // Run all tests in parallel.
   fullyParallel: true,
