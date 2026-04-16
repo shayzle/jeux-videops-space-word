34dc2e42b9477656528689e689246d4125f89f82
Shanisya Lahida <shanisya.lahida@epitech.eu>
2026-04-16 12:12:59 +0200

    test : uniting test for game/personal

--- tests/gaming.test.js
+++ tests/gaming.test.js
@@ -4,49 +4,48 @@ import { getRandomInt, rectIntersect, circleIntersect, timeToString, generateStr
 
 
 
-// PDF Functions
+// PDF Functions (All)
 
-
-describe('getRandomInt', () => {
-  it('getRandomInt(-42, 42) < 43 returns true', () => {
+describe("getRandomInt", () => {
+  it("getRandomInt(-42, 42) < 43 returns true", () => {
     expect(getRandomInt(-42, 42) < 43).toBe(true);
   });
 
-  it('getRandomInt(42, 42) returns 42', () => {
+  it("getRandomInt(42, 42) returns 42", () => {
     expect(getRandomInt(42, 42)).toBe(42);
   });
 });
 
 
-describe('rectIntersect', () => {
-  it('rectIntersect(1,1,2,1,4,1,1,2) returns false', () => {
+describe("rectIntersect", () => {
+  it("rectIntersect(1,1,2,1,4,1,1,2) returns false", () => {
     expect(rectIntersect(1, 1, 2, 1, 4, 1, 1, 2)).toBe(false);
   });
 
-  it('rectIntersect(1,1,5,2,4,1,1,2) returns true', () => {
+  it("rectIntersect(1,1,5,2,4,1,1,2) returns true", () => {
     expect(rectIntersect(1, 1, 5, 2, 4, 1, 1, 2)).toBe(true);
   });
 });
 
 
-describe('circleIntersect', () => {
-  it('circleIntersect(3,2,1,6,1,1.5) returns false', () => {
+describe("circleIntersect", () => {
+  it("circleIntersect(3,2,1,6,1,1.5) returns false", () => {
     expect(circleIntersect(3, 2, 1, 6, 1, 1.5)).toBe(false);
   });
 
-  it('circleIntersect(3,2,1,3,-2,4) returns true', () => {
+  it("circleIntersect(3,2,1,3,-2,4) returns true", () => {
     expect(circleIntersect(3, 2, 1, 3, -2, 4)).toBe(true);
   });
 });
 
 
-describe('timeToString', () => {
-  it('timeToString(123456789) returns "17:36:78"', () => {
-    expect(timeToString(123456789)).toBe('17:36:78');
+describe("timeToString", () => {
+  it("timeToString(123456789) returns '17:36:78'", () => {
+    expect(timeToString(123456789)).toBe("17:36:78");
   });
 
-  it('timeToString("toto") returns "NaN:NaN:NaN"', () => {
-    expect(timeToString('toto')).toBe('NaN:NaN:NaN');
+  it("timeToString('toto') returns 'NaN:NaN:NaN'", () => {
+    expect(timeToString("toto")).toBe("NaN:NaN:NaN");
   });
 });
 
@@ -54,3 +53,8 @@ describe('timeToString', () => {
 
 // Game Functions -> personal
 
+describe("sike", () => {
+  it("sike", () => {
+    expect(sike("do you really think that i'll let you go easy? PLAY THE GAME!")).toBe(true);
+  });
+});
