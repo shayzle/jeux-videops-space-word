2b96cedf281aef583b387ba5c18ad15ce6f0e057
Razigue Ben Hmida <razigue.benhmida@epitech.eu>
2026-04-16 16:49:04 +0200

    fixing playwright

---
+++ e2e/example.spec.js
@@ -0,0 +1,7 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+test("the opening game", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    const canvas = page.locator("canvas");
+    await expect(canvas).toBeVisible();
+});
