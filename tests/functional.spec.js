fd8a5f32bb8e79181ca221e858444e7bc6b98b85
Shanisya Lahida <shanisya.lahida@epitech.eu>
2026-04-16 15:34:59 +0200

    test: functional test with playwright

--- tests/functional.spec.js
+++ tests/functional.spec.js
@@ -1,16 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//     await page.goto('https://playwright.dev/');
-
-//     await expect(page).toHaveTitle(/Playwright/);
-// });
-
-
 test("the opening game", async ({ page }) => {
     await page.goto("/");
     const canvas = page.locator("canvas");
     await expect(canvas).toBeVisible();
-    await expect(button).toHaveURL();
 })
 
