3b5215778fdde014c4135d560aa691a07293fe43
Shanisya Lahida <shanisya.lahida@epitech.eu>
2026-04-17 08:49:35 +0200

    test : my functional tests

--- tests/functional.spec.js
+++ tests/functional.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from 'playwright/test';
 
+
 test("the opening game", async ({ page }) => {
     await page.clock.install();
     await page.goto("http://localhost:3000/");
@@ -7,6 +8,7 @@ test("the opening game", async ({ page }) => {
     const canvas = page.locator("canvas");
     await expect(canvas).toBeVisible();
 })
+
 
 test("the play and quit button", async ({ page }) => {
     await page.clock.install();
@@ -18,20 +20,20 @@ test("the play and quit button", async ({ page }) => {
     await expect(page.locator('#midia-btn')).toBeVisible();
 })
 
+
 test("starting/playing the game", async ({ page }) => {
     await page.clock.install();
     await page.goto("http://localhost:3000/");
 
     await page.locator('#jogar-btn').click();
     
-    // cronometer
     const cronometer = page.locator('.cronometer');
     await expect(cronometer).toBeVisible();
-    // lifebar
+
     const lifebar = page.locator('.lifebar');
     await expect(lifebar).toBeVisible();
     await expect(lifebar).not.toBeEmpty();
-    // text
+
     const text = page.locator('.text');
     await expect(text).toBeVisible();
     await expect(text).not.toBeEmpty();
